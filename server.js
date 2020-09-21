df46420033488a169bea074d458c7ad9e6480225
envalio <40352820+envalio@users.noreply.github.com>
2020-09-21 12:37:49 +0300

    Update server.js

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const MongoClient = require('mongodb').MongoClient;
 const assert = require('assert');
+const ObjectID = require('mongodb').ObjectID;
 const app = express();
 const url = 'mongodb://localhost:27017';
 
@@ -10,27 +11,12 @@ let db;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded( { extended: true } ));
 
-let heroes = [
-    {
-        id: 1,
-        nickname: 'Superman'
-    },
-    {
-        id: 2,
-        nickname: 'Batman'
-    },
-    {
-        id: 3,
-        nickname: 'Aquaman'
-    }
-]
-
-app.get("/", function (req, res) {
+app.get("/", (req, res) => {
     res.send(heroes);
 })
 
-app.get("/heroes", function (req, res) {
-    db.collection('heroes').find().toArray(function (err, docs) {
+app.get("/heroes", (req, res) => {
+    db.collection('heroes').find().toArray((err, docs) => {
         if(err) {
             console.log(err);
             return res.sendStatus(500);
@@ -39,51 +25,72 @@ app.get("/heroes", function (req, res) {
     })
 })
 
-app.post('/heroes', function (req, res) {
-    let hero = {
-      nickname: req.body.nickname,
-      realname: req.body.realname,
-      description: req.body.description,
-      superpower: req.body.superpower,
-      catchphrase: req.body.catchphrase
+app.get("/heroes/:id", (req, res) => {
+    db.collection("heroes").findOne({ _id: ObjectID(req.params.id) }, (err, doc) => {
+        if(err) {
+            console.log(err);
+            return res.sendStatus(500);
+        }
+    })
+    res.send(doc);
+})
+
+app.post('/heroes', (req, res) => {
+    const hero = {
+        nickname: req.body.nickname,
+        realname: req.body.realname,
+        description: req.body.description,
+        superpower: req.body.superpower,
+        catchphrase: req.body.catchphrase
     };    
-    db.collection('heroes').insert(hero, function(err, result) {
+    db.collection('heroes').insert(hero, (err, result) => {
         if (err) {
             console.log(err);
             return res.sendStatus(500);
         }
         res.send(hero);
-    } )
-    
+    } )    
   })
 
-app.put('/heroes/:id', function (req, res) {
-    let hero = heroes.find(function (hero) {
-        return hero.id === Number(req.params.id)
-    });
-    hero.nickname = req.body.nickname;
-    hero.realname = req.body.realname;
-    hero.description = req.body.description;
-    hero.superpower = req.body.superpower;
-    hero.catchphrase = req.body.catchphrase;
-    res.sendStatus(200);
+app.put('/heroes/:id', (req, res) => {
+    db.collection("heroes").updateOne(
+        { _id: ObjectID(req.params.id) },
+        {
+            nickname: req.body.nickname,
+            realname: req.body.realname,
+            description: req.body.description,
+            superpower: req.body.superpower,
+            catchphrase: req.body.catchphrase 
+        },
+        (err, result) => {
+            if(err) {
+                console.log(err);
+                return res.sendStatus(500);
+            }
+            res.sendStatus(200);
+        }
+    )
 })
 
-app.delete('/heroes/:id', function (req, res) {
-    heroes = heroes.filter(function (hero) {
-      return hero.id !== Number(req.params.id)
-    })
-    res.sendStatus(200);
+app.delete('/heroes/:id', (req, res) => {
+    db.collection('heroes').deleteOne(
+        { _id: ObjectID(req.params.id) },
+        (err, result) => {
+          if (err) {
+            console.log(err);
+            return res.sendStatus(500);
+          }
+        res.sendStatus(200);
+      })
+    
 })
-
-
 
 MongoClient.connect(url, (err, client) => {
     assert.equal(null, err);
     db = client.db('jsnapi');
     console.log("Connected successfully to server");
-    app.listen(3030, function() {
+    app.listen(3030, () => {
         console.log('api app works')
     });
-    client.close();
+
 })
