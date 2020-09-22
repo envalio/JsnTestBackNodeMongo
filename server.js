8c27cf7d0f5b27de8c6bd6af590495f99239352d
envalio <40352820+envalio@users.noreply.github.com>
2020-09-22 12:45:01 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,20 +1,21 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const MongoClient = require('mongodb').MongoClient;
-const assert = require('assert');
-const ObjectID = require('mongodb').ObjectID;
-const app = express();
-const url = 'mongodb://localhost:27017';
+const express = require('express'); //connect framework exxpress
+const bodyParser = require('body-parser'); //connect bodyParse fo nodejs
+const MongoClient = require('mongodb').MongoClient; //connect to MongoClient in mongodb
+const assert = require('assert'); //connect assert module
+const ObjectID = require('mongodb').ObjectID; //connect module ObjectID for mongodb
+const app = express(); 
+const url = 'mongodb://localhost:27017'; 
 
 let db;
-
+/*for json: application/json
+and formData: application/x-www-form-urlencoded*/
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded( { extended: true } ));
 
 app.get("/", (req, res) => {
     res.send(heroes);
 })
-
+//method for viewing all HERO in DB
 app.get('/heroes', function (req, res) {
     db.collection('heroes').find().toArray(function (err, docs) {
       if (err) {
@@ -24,7 +25,7 @@ app.get('/heroes', function (req, res) {
       res.send(docs);
     })
   })
-
+//method for find and view some choisen one hero
   app.get('/heroes/:id', function (req, res) {
     db.collection('heroes').findOne({ _id: ObjectID(req.params.id) }, function (err, doc) {
       if (err) {
@@ -34,14 +35,14 @@ app.get('/heroes', function (req, res) {
       res.send(doc);
     })
   })
-
+//method for create new hero card
 app.post('/heroes', (req, res) => {
     const hero = {
-        nickname: req.body.nickname,
-        realname: req.body.realname,
-        description: req.body.description,
-        superpower: req.body.superpower,
-        catchphrase: req.body.catchphrase
+        nickname: req.body.nickname.toString(),
+        realname: req.body.realname.toString(),
+        description: req.body.description.toString(),
+        superpower: req.body.superpower.toString(),
+        catchphrase: req.body.catchphrase.toString()
     };    
     db.collection('heroes').insert(hero, (err, result) => {
         if (err) {
@@ -51,18 +52,19 @@ app.post('/heroes', (req, res) => {
         res.send(hero);
     } )    
   })
-
+//method for upload and change information in choisen hero card
   app.put('/heroes/:id', (req, res) => {
     db.collection('heroes').update(
         { _id: ObjectID(req.params.id) },
         {
-            nickname: req.body.nickname,
-            realname: req.body.realname,
-            description: req.body.description,
-            superpower: req.body.superpower,
-            catchphrase: req.body.catchphrase,
+            $set: { nickname: req.body.nickname,
+                    realname: req.body.realname,
+                    description: req.body.description,
+                    superpower: req.body.superpower,
+                    catchphrase: req.body.catchphrase}
             
         },
+        {  upsert: false  },
         function (err, result) {
             if (err) {
                 console.log(err);
@@ -72,7 +74,7 @@ app.post('/heroes', (req, res) => {
       }
     )
 })
-
+//method for delete choisen hero card
 app.delete('/heroes/:id', (req, res) => {
     db.collection('heroes').deleteOne(
         { _id: ObjectID(req.params.id) },
@@ -85,7 +87,7 @@ app.delete('/heroes/:id', (req, res) => {
       })
     
 })
-
+//connecting MongoClient and mongodb
 MongoClient.connect(url, (err, client) => {
     assert.equal(null, err);
     db = client.db('jsnapi');
